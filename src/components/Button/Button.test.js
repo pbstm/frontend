eb71fb69b43dcdd94ff81c0fa459b3607292c894
Anna Domaskina <taienametztli@mail.ru>
2021-08-11 16:06:41 +0300

    added Button render test

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Button } from './index.js'
 
-let component, mockCallback;
+let component, mockCallback, text;
 
 describe('Button tests', () => {
     beforeEach(
       () => (
+        (text = 'testText'),
         (mockCallback = jest.fn()),
-        (component = shallow(<Button onClick={mockCallback}>Text</Button>))
+        (component = shallow(<Button onClick={mockCallback} text={text}></Button>))
       )
     )
     test('Callback should be called', () => {
       component.find('button').simulate('click')
       expect(mockCallback).toHaveBeenCalled()
     })
+    test('should render correctly', () => {
+        expect(component.find('button')).toHaveLength(1);
+        expect(component.text()).toEqual('testText');
+    });
   })
