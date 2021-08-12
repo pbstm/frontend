319441ec4cf06a1528b899141c5ec3967c6ce35d
Anna Domaskina <taienametztli@mail.ru>
2021-08-12 14:51:19 +0300

    refactored callback NameForm

--- src/pages/Profile/NameForm/index.js
+++ src/pages/Profile/NameForm/index.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types'
 import styles from '../../../components/FormsControls.module.scss'
 import classes from '../Profile.module.scss'
 
-const NameForm = ({ userName, onSubmitName }) => (
-  <div className={classes.FieldContainer}>
-    <div className={classes.FieldTitle}>Name: </div>
-    <div className={styles.formsControls}>
-      <input onBlur={onSubmitName} defaultValue={userName} />
+const NameForm = ({ userName, onSubmitName }) => {
+  const changeNameHandler = (formData) => {
+    const name = formData.target.value
+    if (userName !== name) {
+      onSubmitName(name)
+    }
+  }
+  return (
+    <div className={classes.FieldContainer}>
+      <div className={classes.FieldTitle}>Name: </div>
+      <div className={styles.formsControls}>
+        <input onBlur={changeNameHandler} defaultValue={userName} />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default NameForm
 
