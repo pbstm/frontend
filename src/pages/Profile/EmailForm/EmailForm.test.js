614db2e9a06507abc2f2ab7fbb5c32e77b1507ab
Anna Domaskina <taienametztli@mail.ru>
2021-08-16 18:48:02 +0300

    Refactored and add new tests for EmailForm

--- src/pages/Profile/EmailForm/EmailForm.test.js
+++ src/pages/Profile/EmailForm/EmailForm.test.js
@@ -2,66 +2,125 @@ import React from 'react'
 import { mount } from 'enzyme'
 import EmailForm from './index.js'
 
-let component, userEmail
+let component,
+  emailInput,
+  userEmail,
+  changeEmailSuccess,
+  changeEmailError,
+  mockCallback
 
-describe('EmailForm component', () => {
+describe('Basic tests', () => {
   beforeEach(
     () => (
       (userEmail = 'vasya@mail.ru'),
-      (component = mount(<EmailForm userEmail={userEmail} />))
+      (component = mount(<EmailForm userEmail={userEmail} />)),
+      (emailInput = component.find({ name: 'email' }))
     )
   )
-
   test('should exist', () => {
     expect(component).toBeTruthy()
   })
-  test('props', () => {
+  test('correct props', () => {
     expect(component.props().userEmail).toEqual(userEmail)
   })
-  test('after render input with name="email" should be displayed', () => {
-    expect(component.find({ name: 'email' }).length).toBe(1)
-  })
-  test('after render input with name="password" should not be displayed', () => {
+  test('initial render', () => {
+    expect(emailInput.length).toBe(1)
     expect(component.find({ name: 'password' }).length).toBe(0)
+    expect(component.find('button').length).toBe(0)
   })
-  test('after render button with type="submit" should not be displayed', () => {
-    expect(component.find({ type: 'submit' }).length).toBe(0)
-  })
-  test('after change email input with name="password" should be displayed', () => {
-    component.find({ name: 'email' }).simulate('change')
+  test('render after onChange input email', () => {
+    emailInput.simulate('change')
+    expect(emailInput.length).toBe(1)
     expect(component.find({ name: 'password' }).length).toBe(1)
-  })
-  test('after change email button with type="submit" should be displayed', () => {
-    component.find({ name: 'email' }).simulate('change')
-    expect(component.find({ name: 'password' }).length).toBe(1)
+    expect(component.find('button').length).toBe(1)
   })
   test('defaultValue of input email equal to userEmail', () => {
-    expect(component.find({ name: 'email' }).prop('defaultValue')).toEqual(
-      userEmail
-    )
+    expect(emailInput.prop('defaultValue')).toEqual(userEmail)
   })
   test('after change email value to incorrect div-wrapper should have class .error', () => {
-    const emailInput = component.find({ name: 'email' })
     emailInput.instance().value = 'vasyamail.ru'
     emailInput.simulate('change').simulate('blur')
     expect(component.find('.formsControls').at(0).hasClass('error')).toBe(true)
   })
   test('div-wrapper of blank password input after onBlur should have class .error', () => {
-    component.find({ name: 'email' }).simulate('change')
+    emailInput.simulate('change')
     component.find({ name: 'password' }).simulate('blur')
     expect(component.find('.formsControls').at(1).hasClass('error')).toBe(true)
   })
 })
 
 describe('Incorrect initial email', () => {
-  beforeEach(
-    () => (
-      (userEmail = 'vasyamail.ru'),
-      (component = mount(<EmailForm userEmail={userEmail} />))
-    )
-  )
   test('div-wrapper of input with incorrect email afret onBlur should have class .error', () => {
+    userEmail = 'vasyamail.ru'
+    component = mount(<EmailForm userEmail={userEmail} />)
     component.find({ name: 'email' }).simulate('blur')
     expect(component.find('.formsControls').hasClass('error')).toBe(true)
+  })
+})
+
+describe('Render with success message', () => {
+  beforeEach(
+    () => (
+      (changeEmailSuccess = 'Success Message'),
+      (userEmail = 'vasya@mail.ru'),
+      (component = mount(
+        <EmailForm
+          userEmail={userEmail}
+          changeEmailSuccess={changeEmailSuccess}
+        />
+      ))
+    )
+  )
+  test('render correctly', () => {
+    expect(component.find({ name: 'email' }).length).toBe(1)
+    expect(component.find('.formSummarySuccess').length).toBe(1)
+    expect(component.find({ name: 'password' }).length).toBe(0)
+    expect(component.find('button').length).toBe(0)
+  })
+  test('div with success message should contains correct message', () => {
+    expect(component.find('.formSummarySuccess').text()).toEqual(
+      'Success Message'
+    )
+  })
+})
+
+describe('Render with error message', () => {
+  beforeEach(
+    () => (
+      (changeEmailError = 'Error Message'),
+      (userEmail = 'vasya@mail.ru'),
+      (component = mount(
+        <EmailForm userEmail={userEmail} changeEmailError={changeEmailError} />
+      ))
+    )
+  )
+  beforeEach(
+    //imitate editMode
+    () => component.find({ name: 'email' }).simulate('change')
+  )
+  test('render correctly', () => {
+    expect(component.find({ name: 'email' }).length).toBe(1)
+    expect(component.find('.formSummaryError').length).toBe(1)
+    expect(component.find({ name: 'password' }).length).toBe(1)
+    expect(component.find('button').length).toBe(1)
+  })
+  test('div with error message should contains correct message', () => {
+    expect(component.find('.formSummaryError').text()).toEqual('Error Message')
+  })
+})
+
+describe('Callback tests', () => {
+  beforeEach(
+    () => (
+      (userEmail = 'vasya@mail.ru'),
+      (mockCallback = jest.fn()),
+      (component = mount(
+        <EmailForm userEmail={userEmail} onSubmitEmail={mockCallback} />
+      ))
+    )
+  )
+  test('Callback should be called on submit form', () => {
+    component.find('form').simulate('submit', {})
+    expect(mockCallback).toHaveBeenCalledTimes(1)
   })
 })
