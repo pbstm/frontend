4ffa7c3a1e1d9325f2a33d8c3087ad4457200338
Anna Domaskina <taienametztli@mail.ru>
2021-08-30 16:26:57 +0300

    del redux-form from store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,13 +1,11 @@
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
-import { reducer as formReducer } from 'redux-form'
 import thunkMiddleware from 'redux-thunk'
 import authReducer from './authReducer'
 import changeProfileReducer from './changeProfileReducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  profile: changeProfileReducer,
-  form: formReducer
+  profile: changeProfileReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
