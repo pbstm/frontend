ad1a952442f398249e2d42dff91bd46ad98c7408
Anna Domaskina <taienametztli@mail.ru>
2021-08-31 17:45:23 +0300

    installed i18n

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import MainContainer from '../../hoc/MainContainer'
 
-const Main = () => (
-  <MainContainer>
-    <h1>Main page</h1>
-  </MainContainer>
-)
+const Main = () => {
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language)
+  }
+  return (
+    <MainContainer>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('ru')}>RU</button>
+      <hr />
+      <div>
+        <h1>{t('title')}</h1>
+      </div>
+      <div>{t('description.part1')}</div>
+      <div>{t('description.part2')}</div>
+    </MainContainer>
+  )
+}
 
 export default Main
