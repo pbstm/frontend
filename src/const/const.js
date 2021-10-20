5bf71e297bf2bf03bc18608b41839bdc4e9001b2
Anna Domaskina <taienametztli@mail.ru>
2021-10-20 15:33:52 +0300

    change localhost const

--- src/const/const.js
+++ src/const/const.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const getAccessToken = () => localStorage.token
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3010/api/v1',
+  baseURL: 'http://localhost:3000/api/v1',
   headers: {
     Accept: 'application/json',
     Authorization: getAccessToken()
